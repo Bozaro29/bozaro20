a46c612392a1d1d6a07ea1b37843b10175206756
Bozaro29 <149728434+Bozaro29@users.noreply.github.com>
2023-12-06 20:55:18 +0100

    Add files via upload

---
+++ nikola domasni javascript/Homework1/app.js
@@ -0,0 +1,19 @@
+ console.log("Script is working");
+
+ let pricePerPhone = 119.95;
+ let numberOfPhones = 30;
+ let taxRate = 5;
+
+ let resultnumberOfPhones = pricePerPhone * taxRate / 100;
+
+  console.log(resultnumberOfPhones);
+
+ let  finalResult = resultnumberOfPhones * 30;
+ 
+ console.log(finalResult);
+
+ 
+
+
+ 
+
